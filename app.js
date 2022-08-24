2f45e20df5c9bcfb2b0ed8f91f2f54018330d163
Suraj Kumar <sksuraj01@gmail.com>
2022-08-24 10:35:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome home<h1>");
+})
+
+
+app.listen(2424,()=>{
+    console.log("server started on port 2424");
+})
